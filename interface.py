2f863718ce0795dc18a3d802d8e040b96cae79a3
vilhjalmurkari <vilhjalmurkj@gmail.com>
2022-04-26 22:31:38 +0200

    calculate possibility order

--- interface.py
+++ interface.py
@@ -6,11 +6,66 @@ from sympy import to_cnf, SympifyError
 from BeliefBase import BeliefBase
 
 
+##
+# variables a user can use as for now: p and q 
+##
+logic = ['p','q']
+
+allBeliefs = BeliefBase()
+
+
 def menu():
     print("press button to do action: ")
     print('m: menu')
     print('a: add belief')
     print('p: print all beliefs')
+
+
+def checkPossibilityOrder():
+    # prin()
+    logical_things = []
+    belief_possibility = {}
+
+    for i in range(2**len(logic)):
+        line = ''
+        for j in range(len(logic)):
+            if i//2**j%2 == 0:
+                line += logic[j].lower()
+            else:
+                line += logic[j].upper()
+        logical_things.append(line)
+        belief_possibility[line] = []
+
+        print(f'\t{line}', end='')
+    print()
+    print()
+    # print(logical_things)
+
+    
+    for bs in allBeliefs.beliefsSet:
+        print(f'{bs.belief}\t' , end='')
+        for log in logical_things:
+            # print(f'{log}\t', end='')
+            val = 0
+            for l in log:
+                if l in str(bs.belief):
+                    val = 1
+            
+            belief_possibility[log].append(val)
+            print(f'{val}\t', end='')
+
+        # belief_possibility[bs.belief] = pos_list.copy()
+        print()
+    
+    for th in logical_things:
+        pos = sum(belief_possibility[th])/len(belief_possibility[th])
+        print(f'\t%.2f' % pos, end='')
+
+
+    # print(allBeliefs.beliefsSet)
+
+
+
 
 
 def interfaceLoop(allBeliefs):
