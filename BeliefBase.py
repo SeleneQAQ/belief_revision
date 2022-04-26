76d7dc6c74b405e72c88ad3efa3726f4a5d83b21
Selene <502440118@qq.com>
2022-04-26 19:21:36 +0200

    1.add function of deleteSameBelief, which will replace old belief by new belief which are the same in logic (like input p>>q, we will delete ~p|q) 2.now we storage belief as what we input, not change it into CNF - only when we compare or do resolution we need to use CNF, and I think we should to storage belief as what we input

--- BeliefBase.py
+++ BeliefBase.py
@@ -3,18 +3,33 @@ import math
 import logging
 from operator import neg
 
-from sympy.logic.boolalg import to_cnf, And, Or, Equivalent
+from sympy.logic.boolalg import to_cnf, And, Or, Equivalent, Implies
 from sortedcontainers import SortedList
 from Belief import Belief
 
 class BeliefBase:
     def __init__(self):
-        self.beliefsSet = [] 
+        self.beliefsSet = []
 
     def addBelief(self, belief, plausibilityOrder):
-        formula = to_cnf(belief)
-        x = Belief(formula, plausibilityOrder)
-        self.beliefsSet.append(x)
+        if self.deleteSameBelief(belief, plausibilityOrder) == 1:
+            x = Belief(belief, plausibilityOrder)
+            self.beliefsSet.append(x)
+
+    def deleteSameBelief(self, newBelief, plausibilityOrder):
+        for belief in self.beliefsSet:
+            if to_cnf(belief.belief) == to_cnf(newBelief):
+                belief.belief = newBelief
+                belief.plausibilityOrder = plausibilityOrder
+                return 0
+        return 1
+
+
+
+    def resolution(self, belief, plausibilityOrder):
+        formula = to_cnf('~('+belief+')')
+        print(formula)
+
 
     def __repr__(self):
         if len(self.beliefsSet) == 0:
