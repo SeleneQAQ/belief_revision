2e2c41fdd869bbda3019766ffb7af43acfae065c
Selene <502440118@qq.com>
2022-04-26 21:21:17 +0200

    1.add function of deleteSameBelief, which will replace old belief by new belief which are the same in logic (like input p>>q, we will delete ~p|q) 2.now we storage belief as what we input, not change it into CNF - only when we compare or do resolution we need to use CNF, and I think we should to storage belief as what we input 3.now we can divide our belief set by different op(like And/Or)

--- BeliefBase.py
+++ BeliefBase.py
@@ -1,11 +1,11 @@
-
 import math
 import logging
 from operator import neg
 
-from sympy.logic.boolalg import to_cnf, And, Or, Equivalent, Implies
+from sympy.logic.boolalg import to_cnf, And, Or, Equivalent, Implies, disjuncts
 from sortedcontainers import SortedList
 from Belief import Belief
+
 
 class BeliefBase:
     def __init__(self):
@@ -16,6 +16,7 @@ class BeliefBase:
             x = Belief(belief, plausibilityOrder)
             self.beliefsSet.append(x)
 
+    #when a new belief come, check if it has same in the original part
     def deleteSameBelief(self, newBelief, plausibilityOrder):
         for belief in self.beliefsSet:
             if to_cnf(belief.belief) == to_cnf(newBelief):
@@ -24,17 +25,30 @@ class BeliefBase:
                 return 0
         return 1
 
-
-
     def resolution(self, belief, plausibilityOrder):
-        formula = to_cnf('~('+belief+')')
-        print(formula)
+        formula = to_cnf(belief)
+        anti_formula = to_cnf('~(' + belief + ')')
+        print([formula])
+        element = self.divideElement([formula], And)
+        for i in element:
+            print(i)
 
+
+    # divide belief by op
+    def divideElement(self, ori, op):
+        result = []
+
+        def divide(subargs):
+            for arg in subargs:
+                if isinstance(arg, op):
+                    divide(arg.args)
+                else:
+                    result.append(arg)
+
+        divide(ori)
+        return result
 
     def __repr__(self):
         if len(self.beliefsSet) == 0:
             return 'empty'
         return '\n'.join(str(x) for x in self.beliefsSet)
-
-
-
