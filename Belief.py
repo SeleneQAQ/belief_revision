150d484f28eac663d07757c19dcc18b3df242bb4
Piotr <ps>
2022-04-27 15:25:54 +0200

    plausibilityOrder automatically calculated

--- Belief.py
+++ Belief.py
@@ -1,8 +1,8 @@
 
 class Belief:
-    def __init__(self, belief,  plausibilityOrder):
+    def __init__(self, belief):
         self.belief = belief
-        self.plausibilityOrder = plausibilityOrder
+        self.plausibilityOrder = 1.0
 
     def __repr__(self):
         return f'Belief({self.belief}, plausibilityOrder={self.plausibilityOrder})'
