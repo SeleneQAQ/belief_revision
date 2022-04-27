a0f9266cfea83670cd0da31fd3ca3bda183fd0f0
Piotr <ps>
2022-04-27 15:28:47 +0200

    revise and add fixed: allow no duplicates

--- BeliefBase.py
+++ BeliefBase.py
@@ -12,31 +12,50 @@ class BeliefBase:
         self.beliefsSetOriginal = []
         self.beliefsSetCNF = []
 
-    def addBelief(self, belief, plausibilityOrder):
-        if self.deleteSameBelief(belief, plausibilityOrder) == 1:
-            x = Belief(belief, plausibilityOrder)
+    def addBelief(self, belief):
+        if self.deleteSameBelief(belief) == 1:
+            x = Belief(belief)
             self.beliefsSetOriginal.append(x)
+            self.calcutatePlausibilityOrders(self.beliefsSetOriginal)
+
+    def calcutatePlausibilityOrders(self, beliefsSet):
+        if len(beliefsSet) == 1:
+            pass
+        else:
+            ratio = 1/len(beliefsSet)
+            i = 0
+            for i in range(0, len(beliefsSet)):
+                beliefsSet[i].plausibilityOrder = (i+1)*ratio
+
+            
 
     #when a new belief come, check if it has same in the original part
-    def deleteSameBelief(self, newBelief, plausibilityOrder):
+    def deleteSameBelief(self, newBelief):
+        
         for belief in self.beliefsSetOriginal:
             if to_cnf(belief.belief) == to_cnf(newBelief):
-                belief.belief = newBelief
-                belief.plausibilityOrder = plausibilityOrder
+                #belief.belief = newBelief
+                #belief.plausibilityOrder = 1.0
+                self.beliefsSetOriginal.remove(belief)
+                self.addBelief(newBelief)
                 return 0
+        
         return 1
 
-    def resolution(self, belief, plausibilityOrder):
+    def resolution(self, belief):
         formula = to_cnf(belief)
         anti_formula = to_cnf('~(' + belief + ')')
         print([formula])
 
-        self.beliefsSetOriginal.append(Belief(belief, plausibilityOrder))
-        self.beliefsSetCNF.append(Belief(formula, plausibilityOrder))
-
-        element = self.divideElement([formula], And)
-        for i in element:
-            print(i)
+        
+        if self.deleteSameBelief(belief) == 1:
+            x = Belief(belief)
+            self.beliefsSetOriginal.append(x)
+            self.beliefsSetCNF.append(Belief(formula))
+            element = self.divideElement([formula], And)
+            self.calcutatePlausibilityOrders(self.beliefsSetOriginal)
+            for i in element:
+                print(i)
 
 
     # divide belief by op
